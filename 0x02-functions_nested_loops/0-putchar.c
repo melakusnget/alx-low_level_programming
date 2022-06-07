0e8aa7d0ee60860d31570ce1cba398fb00d459cd
melakusnget <105011538+melakusnget@users.noreply.github.com>
2022-06-07 20:55:32 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,45 +1,24 @@
-#include "main.h" 
+#include "main.h"
 
-  
+/**
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
 
-  
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
- /** 
-
-  * main - Entry point 
-
-  * 
-
-  * Return: nothing, return void 
-
-  */ 
-
-  
-
- int main(void) 
-
- { 
-
- _putchar('_'); 
-
- _putchar('p'); 
-
- _putchar('u'); 
-
- _putchar('t'); 
-
- _putchar('c'); 
-
- _putchar('h'); 
-
- _putchar('a'); 
-
- _putchar('r'); 
-
- _putchar('\n'); 
-
-  
-
- return (0); 
-
- }
+	return (0);
+}
